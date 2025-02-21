be978c077e970fb9583ca42043b11c51148243d8
Ho Yeung Lai <johnl4112@gmail.com>
2025-02-21 17:28:54 +0800

    default url added

---
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6resttemplate.client;
+
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Service
+public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
+    @Override
+    public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate =restTemplateBuilder.build();
+
+        ResponseEntity<BeerDTOPageImpl> responseEntity = restTemplate
+                .getForEntity(GET_BEER_PATH, BeerDTOPageImpl.class);
+
+        return responseEntity.getBody();
+    }
+}
