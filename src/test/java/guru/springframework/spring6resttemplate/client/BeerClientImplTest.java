035739d52df0082becc30cb0c01143705a09d9b5
Ho Yeung Lai <johnl4112@gmail.com>
2025-03-03 09:55:36 +0800

    temp

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,8 +15,23 @@ class BeerClientImplTest {
     BeerClientImpl beerClient;
 
     @Test
-    void listBeers() {
+    void testGetBeerById() {
+        Page<BeerDTO> beerDTOs = beerClient.listBeers();
 
-        beerClient.listBeers();
+        BeerDTO beerDTO = beerDTOs.getContent().getFirst();
+
+        BeerDTO byId = beerClient.getBeerById(beerDTO.getId());
+
+        assertNotNull(byId);
+    }
+
+    @Test
+    void testListBeersWithNoBeerName() {
+        beerClient.listBeers(null, null, null, null, null);
+    }
+
+    @Test
+    void testListBeers() {
+        beerClient.listBeers("ALE", null, null, null, null);
     }
 }
