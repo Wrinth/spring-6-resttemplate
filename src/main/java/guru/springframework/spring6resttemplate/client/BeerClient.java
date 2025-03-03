ffbfc357872206ade3034c31838a0dab66480829
Ho Yeung Lai <johnl4112@gmail.com>
2025-03-03 10:25:05 +0800

    restTemplate added

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -9,7 +9,14 @@ import java.util.UUID;
 public interface BeerClient {
     Page<BeerDTO> listBeers();
 
-    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize);
+    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
+                            Integer pageSize);
 
     BeerDTO getBeerById(UUID beerId);
+
+    BeerDTO createBeer(BeerDTO newDto);
+
+    BeerDTO updateBeer(BeerDTO beerDto);
+
+    void deleteBeer(UUID beerId);
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,14 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,23 +19,81 @@ class BeerClientImplTest {
     BeerClientImpl beerClient;
 
     @Test
-    void testGetBeerById() {
-        Page<BeerDTO> beerDTOs = beerClient.listBeers();
+    void testDeleteBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs 2")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
 
-        BeerDTO beerDTO = beerDTOs.getContent().getFirst();
+        BeerDTO beerDto = beerClient.createBeer(newDto);
 
-        BeerDTO byId = beerClient.getBeerById(beerDTO.getId());
+        beerClient.deleteBeer(beerDto.getId());
+
+        assertThrows(HttpClientErrorException.class, () -> {
+            //should error
+            beerClient.getBeerById(beerDto.getId());
+        });
+    }
+
+    @Test
+    void testUpdateBeer() {
+
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs 2")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO beerDto = beerClient.createBeer(newDto);
+
+        final String newName = "Mango Bobs 3";
+        beerDto.setBeerName(newName);
+        BeerDTO updatedBeer = beerClient.updateBeer(beerDto);
+
+        assertEquals(newName, updatedBeer.getBeerName());
+    }
+
+    @Test
+    void testCreateBeer() {
+
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO savedDto = beerClient.createBeer(newDto);
+        assertNotNull(savedDto);
+    }
+
+    @Test
+    void getBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
 
         assertNotNull(byId);
     }
 
     @Test
-    void testListBeersWithNoBeerName() {
+    void listBeersNoBeerName() {
+
         beerClient.listBeers(null, null, null, null, null);
     }
 
     @Test
-    void testListBeers() {
+    void listBeers() {
+
         beerClient.listBeers("ALE", null, null, null, null);
     }
 }
